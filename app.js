17ce5171efd4a6cef6a48dbc7d6a2183a36a6dc7
Marlon <marlonfsolis@outlook.com>
2020-04-14 22:19:25 -0400

    Add MongoDB config

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+require('./startup/db')();
 require('./startup/routes')(app);
 
 module.exports = app;
--- startup/db.js
+++ startup/db.js
@@ -1,1 +1,30 @@
+// mongoose.js configuration
+const config = require('config');
 const mongoose = require('mongoose');
+const {dbDebug} = require('./debugger');
+
+module.exports = () => {
+    // find the environment var info on RoboForm
+    const username = config.get('db.username');
+    const password = config.get('db.password');
+    const dbname = config.get('db.name');
+    const host = config.get('db.host');
+
+    // https://mongoosejs.com/docs/deprecations.html
+    const conStr = `mongodb://${username}:${password}@${host}/${dbname}`;
+    dbDebug('Connection String:', conStr);
+    mongoose.connect(conStr, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, autoReconnect: true})
+        .then(() => {
+            dbDebug('MongoDB connection completed.');
+        });
+
+    // noinspection JSUnresolvedFunction
+    mongoose.connection.on('error', () => {
+        dbDebug('Error connecting to the database.');
+    });
+
+    // noinspection JSUnresolvedFunction
+    mongoose.connection.once('open', () => {
+        dbDebug('Connection open.');
+    });
+};
