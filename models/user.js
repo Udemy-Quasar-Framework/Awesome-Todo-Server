7280b05b9b266fe6d3a8a71b9737ead1ecee0f1a
Marlon Fernandez <mfernandez@coadvantage.com>
2020-04-15 19:00:01 -0400

    Added login resource.

--- models/user.js
+++ models/user.js
@@ -26,6 +26,25 @@ const _userAuthValidation = () => [
     check('password').exists().matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z@#$%^&()_=?!;:<>.,]{8,}$/, 'i')
 ];
 
+
+// const UserModel = model('user', userSchema);
+
+/**
+ * Class User
+ * @property {string} name - Name of the user.
+ * @property {string} email - User email.
+ * @property {string} password - User password.
+ * @constructor
+ */
+module.exports.UserC = function(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+}
+
+/**
+ * @type {Model<UserC>}
+ */
 module.exports.User = model('user', userSchema);
 module.exports.userValidation = _userValidation();
 module.exports.userAuthValidation = _userAuthValidation();
--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {validationResult} = require('express-validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const Res = require('../models/Res');
 const {Err, getErrorArr} = require('../models/Err');
 const {debug} = require('../startup/debuggers');
-const {User, userValidation} = require('../models/user');
+const {User, UserC, userValidation, userAuthValidation} = require('../models/user');
 
 
 /**
@@ -40,7 +42,7 @@ router.post('/register', userValidation, async (req, res) => {
     /* create the new user */
     errors = null;
     const passwordHash = await bcrypt.hash(password, 10); // more than 10 breaks the method
-    await User.create({name, email, pass: passwordHash}).catch(ex => {
+    await User.create(new UserC(name, email, passwordHash)).catch(ex => {
         debug('Error saving your changes.');
         errors = getErrorArr(ex.errors);
     });
@@ -58,11 +60,47 @@ router.post('/register', userValidation, async (req, res) => {
 /**
  * Login or authenticate a user. Will validate credentials and return a JWT token.
  */
-router.post('/login', async (req, res) => {
-    const token = "asdasdfwewqedwd.dqasd.qweqweqwdeqwd";
+router.post('/login', userAuthValidation, async (req, res) => {
+    let errors = null;
+    const {email, password} = req.body;
+    const errorMsg = 'Email or password not valid.';
+    const credentialError = new Err(errorMsg, 'email, password', email, 'body');
+
+    /* validate body data */
+    errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        debug('Model errors: ', errors);
+        res.statusCode = 400;
+        res.send(new Res(false, 'Request body not valid.', errors.array({onlyFirstError: false})));
+        return;
+    }
+
+    /* validate user */
+    const user = await User.findOne({email});
+    if (!user) {
+        debug(`User with email ${email} not found.`);
+        res.statusCode = 400;
+        res.send(new Res(false, errorMsg, [credentialError]));
+        return;
+    }
+
+    /* validate password */
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+        debug(`The password ${password} is not valid.`);
+        res.statusCode = 400;
+        res.send(new Res(false, errorMsg, errors));
+        return;
+    }
+
+    // Everything fine, authenticate the user
+    const payload = {userId: user._id};
+    const data = {
+        token: jwt.sign(payload, config.get('jwt-secret'), {expiresIn: '1h'})
+    };
 
     res.statusCode = 200;
-    res.send(new Res(true, 'User is authorized.', [], {token}));
+    res.send(new Res(true, 'User is authorized.', [], data));
 });
 
 module.exports = router;
