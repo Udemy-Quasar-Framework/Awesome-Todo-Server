a127e2c26335af2de5ae61dd7ae1ec71b70136c3
Marlon Fernandez <mfernandez@coadvantage.com>
2020-04-15 13:49:52 -0400

    Added the register endpoint. Fixed Err module. Fixed Res module. Removed users resources.

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,7 @@
 const indexRouter = require('../routes/index');
-const usersRouter = require('../routes/users');
+const authRouter = require('../routes/auth');
 
 module.exports = function(app) {
     app.use('/', indexRouter);
-    app.use('/users', usersRouter);
+    app.use('/api/auth', authRouter);
 };
