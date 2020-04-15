ffa98dc3bb9267a3d7624c863ade6b2c2ba3588f
Marlon Fernandez <mfernandez@coadvantage.com>
2020-04-15 10:11:37 -0400

    Add catch to the mongodb connection. This avoid the promise not handler error.

--- startup/db.js
+++ startup/db.js
@@ -5,28 +5,31 @@ const {dbDebug} = require('./debuggers');
 
 module.exports = () => {
     // find the environment var info on RoboForm
-    // const username = config.get('db.username');
-    // const password = config.get('db.password');
-    // const dbname = config.get('db.name');
-    // const host = config.get('db.host');
+    const username = config.get('db.username');
+    const password = config.get('db.password');
+    const dbname = config.get('db.name');
+    const host = config.get('db.host');
 
     // https://mongoosejs.com/docs/deprecations.html
-    // const conStr = `mongodb://${username}:${password}@${host}/${dbname}`;
-    // dbDebug('Connection String:', conStr);
-    dbDebug('Connection String:');
+    const conStr = `mongodb://${username}:${password}@${host}/${dbname}`;
+    dbDebug('Connection String:', conStr);
 
-    // mongoose.connect(conStr, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
-    //     .then(() => {
-    //         dbDebug('MongoDB connection completed.');
-    //     });
 
-    // // noinspection JSUnresolvedFunction
-    // mongoose.connection.on('error', () => {
-    //     dbDebug('Error connecting to the database.');
-    // });
-    //
-    // // noinspection JSUnresolvedFunction
-    // mongoose.connection.once('open', () => {
-    //     dbDebug('Connection open.');
-    // });
+    mongoose.connect(conStr, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+        .then(() => {
+            dbDebug('MongoDB connection completed.');
+        })
+        .catch(() => {
+            dbDebug('MongoDB connection error.');
+        });
+
+    // noinspection JSUnresolvedFunction
+    mongoose.connection.on('error', () => {
+        dbDebug('Error connecting to the database.');
+    });
+
+    // noinspection JSUnresolvedFunction
+    mongoose.connection.once('open', () => {
+        dbDebug('Connection open.');
+    });
 };
