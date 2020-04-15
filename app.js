f2aa401d5f62a0f2152fe54b5f9e1d66d97ec647
Marlon Fernandez <mfernandez@coadvantage.com>
2020-04-15 09:21:14 -0400

    Added the async error handler.

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+require('express-async-errors');
+
+const Res = require('./models/Res');
+const Err = require('./models/Err');
+const {debug} = require('./startup/debuggers');
+
 const app = express();
 
 app.use(logger('dev'));
@@ -13,5 +19,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 require('./startup/db')();
 require('./startup/routes')(app);
+
+// handler 404 requests
+app.use(function(req, res, next) {
+    res.send(new Res(false, '404 - Resource not found.', [
+        new Err('Resource not found', 'unknown', 'unknown', 'unknown')
+    ]));
+});
+
+// handler unexpected errors
+app.use(function(err, req, res, next) {
+    debug('Unhandled error. Error Detail: ', err);
+
+    const errMsg = err && err.message ? err.message : 'Unhandled error.'
+    res.statusCode = 500;
+    res.send(new Res(false, 'Unhandled error occurred.', [
+        new Err(errMsg, 'unknown', 'unknown', 'unknown')
+    ]));
+});
 
 module.exports = app;
