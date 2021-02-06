a87000a190c197ff6ef074f0b0f6cafa546d514a
Marlon Fernandez <marlonfsolis@outlook.com>
2021-02-06 17:04:34 -0500

    Adding Prettier formatter tool

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,11 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require('jsonwebtoken')
+const config = require('config')
 
-const Res = require("../models/Res");
-const {Err} = require("../models/Err");
-const {debug} = require("../startup/debuggers");
-const {Payload, genToken} = require("../models/auth");
-const {User} = require('../models/user');
-
+const Res = require('../models/Res')
+const { Err } = require('../models/Err')
+const { debug } = require('../startup/debuggers')
+const { Payload, genToken } = require('../models/auth')
+const { User } = require('../models/user')
 
 /**
  * Authorization middleware
@@ -15,63 +14,69 @@ const {User} = require('../models/user');
  * @param next
  */
 module.exports.auth = async (req, res, next) => {
-    const token = req.header('X-Auth-Token') || null;
+  const token = req.header('X-Auth-Token') || null
 
-    /* no token validation */
-    if (!token) {
-        debug('Token not provided.');
-        res.statusCode = 401;
-        res.send(new Res(false, 'Token not provided', [
-            new Err('Token not provided', 'X-Auth-Token', token, 'header')
-        ]));
-        return;
+  /* no token validation */
+  if (!token) {
+    debug('Token not provided.')
+    res.statusCode = 401
+    res.send(
+      new Res(false, 'Token not provided', [
+        new Err('Token not provided', 'X-Auth-Token', token, 'header'),
+      ])
+    )
+    return
+  }
+
+  try {
+    const p = await jwt.verify(token, config.get('jwt-secret'))
+    const payload = new Payload(p)
+    const userId = payload.userId
+
+    /* existing token validation */
+    const { lastAuthToken } = await User.findById(userId, '-_id lastAuthToken')
+
+    if (lastAuthToken !== token) {
+      debug(
+        'Invalid token. The token is JWT valid but is not the last token provided.'
+      )
+      const errorMsg =
+        'Invalid Token. The token is not valid for the current session.'
+      res.statusCode = 401
+      res.send(
+        new Res(false, errorMsg, [
+          new Err(errorMsg, 'X-Auth-Token', token, 'header'),
+        ])
+      )
+      return
     }
 
+    /* refresh the token */
     try {
-
-        const p = await jwt.verify(token, config.get('jwt-secret'));
-        const payload = new Payload(p);
-        const userId = payload.userId;
-
-        /* existing token validation */
-        const {lastAuthToken} = await User.findById(userId, "-_id lastAuthToken");
-
-        if (lastAuthToken !== token) {
-            debug('Invalid token. The token is JWT valid but is not the last token provided.');
-            const errorMsg = 'Invalid Token. The token is not valid for the current session.';
-            res.statusCode = 401;
-            res.send(new Res(false, errorMsg, [
-                new Err(errorMsg, 'X-Auth-Token', token, 'header')
-            ]));
-            return;
-        }
-
-        /* refresh the token */
-        try {
-            // TODO Move the genToken to User model
-            const newToken = genToken(p);
-            await User.updateOne({_id:userId}, {lastAuthToken: newToken});
-        } catch (ex) {
-            const errorMsg = 'Error when trying to refresh the token.';
-            res.statusCode = 500;
-            res.send(new Res(false, errorMsg, [
-                new Err(ex.message, 'unknown', 'unknown', 'unknown')
-            ]));
-            return;
-        }
-
-
-        res.setHeader('X-Auth-Token', token);
-        res.locals.payload = payload;
-        next();
-
-    } catch (e) {
-
-        const errorMsg = 'Token provided not valid. It could be that has expired.';
-        res.statusCode = 403;
-        res.send(new Res(false, errorMsg, [
-            new Err(e.message, 'X-Auth-Token', token, 'header')
-        ]));
-
+      // TODO Move the genToken to User model
+      const newToken = genToken(p)
+      await User.updateOne({ _id: userId }, { lastAuthToken: newToken })
+    } catch (ex) {
+      const errorMsg = 'Error when trying to refresh the token.'
+      res.statusCode = 500
+      res.send(
+        new Res(false, errorMsg, [
+          new Err(ex.message, 'unknown', 'unknown', 'unknown'),
+        ])
+      )
+      return
     }
-};
+
+    res.setHeader('X-Auth-Token', token)
+    res.locals.payload = payload
+    next()
+  } catch (e) {
+    const errorMsg = 'Token provided not valid. It could be that has expired.'
+    res.statusCode = 403
+    res.send(
+      new Res(false, errorMsg, [
+        new Err(e.message, 'X-Auth-Token', token, 'header'),
+      ])
+    )
+  }
+}
